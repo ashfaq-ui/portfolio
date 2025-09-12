c4540f2f0596b32d2ff3fbc35be674e8369f391e
ashfaq-ui <rizvimahamed456@gmail.com>
2025-09-12 23:54:17 +0530

    navigation bar strcture added

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import './Navbar.css'
+
+
+export default function Navbar(){
+    return(
+        <>
+            <nav className="navbar">
+
+                <h1>logo</h1>
+
+                <ul className="nav-list">
+                    <li>About</li>
+                    <li>Skill</li>
+                    <li>Projects</li>
+                    <li>Education</li>
+                    <li>contact</li>
+                </ul>
+
+
+                <h1>social media</h1>
+
+            </nav>
+        
+        </>
+    )
+}
